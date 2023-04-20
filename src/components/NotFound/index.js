95caad7db92fc2da158e5d9f07fdebeaf69f62cc
chandru <chandrashekharr355@gmail.com>
2023-04-20 17:09:26 +0000

    IPL Dashboard App

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,10 @@
+// Write your code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-fount-main-container">
+    <p className="not-fount-text">Page Not Found</p>
+  </div>
+)
+
+export default NotFound
